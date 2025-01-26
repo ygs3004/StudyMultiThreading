1aba3326ea8047d78f865b1caa3dd9bf69bf87ee
ygs <ygs3004@naver.com>
2025-01-27 04:02:47 +0900

    쓰레드 생성, Exception Handler

---
+++ src/section2/BasicThread.java
@@ -0,0 +1,22 @@
+package section2;
+
+public class BasicThread {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("New Worker Thread 내부 : " + Thread.currentThread().getName());
+                System.out.println("현재 Thread Priority:" + Thread.currentThread().getPriority());
+            }
+        });
+
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("쓰레드 시작 전 쓰레드: " + Thread.currentThread().getName());
+        thread.start();
+        System.out.println("쓰레드 시작 후 쓰레드: " + Thread.currentThread().getName());
+    }
+
+}
---
+++ src/section2/ThreadExceptionHandlerExample.java
@@ -0,0 +1,26 @@
+package section2;
+
+public class ThreadExceptionHandlerExample {
+
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                throw new RuntimeException("Some Runtime Exception");
+            }
+        });
+
+        thread.setName("Example Thread");
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("Exception in thread: " + t.getName());
+                System.out.println("error: " + e.getMessage());
+            }
+        });
+        thread.start();
+    }
+
+}
